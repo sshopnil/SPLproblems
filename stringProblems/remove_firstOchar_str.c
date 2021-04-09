0e33561dedcf3dbb13e9b0db224d17025ba3d45c
sshopnil <sshopnil@github.com>
2021-04-09 21:00:40 +0600

    updated remv character from a string

--- stringProblems/remove_firstOchar_str.c
+++ stringProblems/remove_firstOchar_str.c
@@ -1,31 +1,79 @@
+// #include<stdio.h>
+// void removechar(char str1[], char c);
+// int main()
+// {
+//     char str1[100], c;
+
+//     printf("Enter string: ");
+//     gets(str1);
+//     printf("Character to remove: ");
+//     scanf("%c", &c);
+
+//     removechar(str1, c);
+    
+//     printf("After removing %c from string is: %s\n", c, str1);
+    
+//     return 0;
+// }
+// void removechar(char str1[], char c)
+// {
+//     int len1, i, j;
+
+//     for(len1 = 0; str1[len1] != '\0'; len1++);
+
+//     for(i = 0; i < len1 && str1[i] != c; i++);
+
+//     while(i < len1)
+//     {
+//         str1[i] = str1[i+1];
+//         i++;
+//     }
+// }
 #include<stdio.h>
-void removechar(char str1[], char c);
-int main()
+int checkSubStr(char str1[], char str2[])
 {
-    char str1[100], c;
-
-    printf("Enter string: ");
-    gets(str1);
-    printf("Character to remove: ");
-    scanf("%c", &c);
-
-    removechar(str1, c);
-    
-    printf("After removing %c from string is: %s\n", c, str1);
-    
-    return 0;
-}
-void removechar(char str1[], char c)
-{
-    int len1, i, j;
+    int i, j, len1, len2;
 
     for(len1 = 0; str1[len1] != '\0'; len1++);
+    for(len2 = 0; str2[len2] != '\0'; len2++);
 
-    for(i = 0; i < len1 && str1[i] != c; i++);
+    for(i = 0; i <= len1-len2; i++)
+    {
+        for(j = 0; j < len2; j++)
+        {
+            if(str1[i+j] != str2[j])
+            {
+                break;
+            }
+        }
+        if(j == len2)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+void rmvSubStr(char str1[], int n)
+{
+    int i, j, len;
+    
+    for(len = 0; str1[len] != '\0'; len++);
 
-    while(i < len1)
+    for(i = n; i < len-n; i++)
     {
         str1[i] = str1[i+1];
-        i++;
     }
+}
+int main()
+{
+    char str1[100], str2[50];
+    int sub_i;
+
+    gets(str1);
+    gets(str2);
+
+    sub_i = checkSubStr(str1, str2);
+    rmvSubStr(str1, sub_i);
+
+    printf("%s", str1);
 }
