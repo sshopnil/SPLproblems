106875bfdff6d8f88ca10c0e7560bbfb2d1533d3
Shayanta Shopnil <65902426+sshopnil@users.noreply.github.com>
2021-05-09 03:05:32 +0600

    Find maximum occurence character (updated in count_occurence)

--- stringProblems/count_occurence.c
+++ stringProblems/count_occurence.c
@@ -20,36 +20,79 @@
 //     printf("Occurence of %c is %d times\n",ch, count);
 // }
 
+
+
+
 //occurence of a substring in a string
 
-#include<stdio.h>
-int const SIZE = 50;
+// #include<stdio.h>
+// int const SIZE = 50;
 
+// int main()
+// {
+//     char str1[SIZE], str2[SIZE];
+//     printf("Enter full string: ");
+//     gets(str1);
+
+//     printf("Enter sub-string: ");
+//     gets(str2);
+
+//     int count = 0, i, j, len;
+
+//     for(len = 0; str2[len] != '\0'; len++);
+
+//     for(i = 0; str1[i] != '\0'; i++)
+//     {
+//         for(j = 0; str2[j] != '\0'; j++)
+//         {
+//             if(str2[j] != str1[i+j])
+//             break;
+//         }
+//         if(str2[j] == '\0')
+//         {
+//             count++;
+//         }
+//     }
+
+//     printf("'%s' occured %d times\n", str2, count);
+// }
+
+
+
+
+//find maximum occurence
+
+#include<stdio.h>
+const int SIZE = 50;
 int main()
 {
-    char str1[SIZE], str2[SIZE];
-    printf("Enter full string: ");
-    gets(str1);
+    char str[SIZE], max;
+    int i, j, max_c = 0, count = 0;
 
-    printf("Enter sub-string: ");
-    gets(str2);
+    gets(str);
 
-    int count = 0, i, j, len;
-
-    for(len = 0; str2[len] != '\0'; len++);
-
-    for(i = 0; str1[i] != '\0'; i++)
+    for(i = 0; str[i] != '\0'; i++)
     {
-        for(j = 0; str2[j] != '\0'; j++)
+        count = 0;
+        if(str[i] >= 65 && str[i] <= 90)
         {
-            if(str2[j] != str1[i+j])
-            break;
+            str[i] -= 65;
+            str[i] += 97;
         }
-        if(str2[j] == '\0')
+        for(j = i+1; str[j] != '\0'; j++)
         {
-            count++;
+            if(str[i] == str[j])
+            {
+                count++;
+            }
+        }
+        if(max_c <= count)
+        {
+            max = str[i];
+            max_c = count;
         }
     }
+    printf("%c (or %c)\n", max-32, max);
 
-    printf("'%s' occured %d times\n", str2, count);
+    return 0;
 }
